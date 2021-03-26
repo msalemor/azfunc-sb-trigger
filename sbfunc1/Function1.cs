720f59acf71eab93299f282cd263a249ed224f00
Alex Morales <alemor@microsoft.com>
2021-03-25 21:15:40 -0400

    Added comments

--- sbfunc1/Function1.cs
+++ sbfunc1/Function1.cs
@@ -11,6 +11,10 @@ namespace sbfunc1
         public static void Run([ServiceBusTrigger("simplequeue", Connection = "SBConnectionString")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+
+            // throw an error to retry
+            // eventually message deadletters
+            // modify host.json: "messageHandlerOptions": "autoComplete": true
         }
     }
 }
